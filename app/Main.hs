e359200c1ff0891fb688c7a1da572dddd051e3c1
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-12-03 22:13:56 +0900

    test

--- app/Main.hs
+++ app/Main.hs
@@ -1,63 +1,7 @@
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE TypeOperators              #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE FlexibleContexts           #-}
-
 module Main where
 
-import           Servant
-import           Servant.Mock
-import           Model
-import           Control.Monad.IO.Class
+import           Api (app)
 import qualified Network.Wai.Handler.Warp as Warp
-import           Database.Persist
-import           Database.Persist.Sqlite
-import           Control.Arrow (left, right)
-import           Data.Text.Lazy.Encoding (encodeUtf8)
-import           Data.Text.Lazy (fromStrict)
-import           Data.ByteString.Lazy.Char8 (pack)
-import           Data.Monoid ((<>))
-
-type UsersAPI = Get '[JSON] [User]
-           :<|> ReqBody '[JSON] User :> Post '[JSON] User
-           :<|> Capture "id" Int :> Get '[JSON] User
-           :<|> Capture "id" Int :> ReqBody '[JSON] User :> Put '[JSON] ()
-           :<|> Capture "id" Int :> Delete '[JSON] ()
-
-userApi :: Proxy UsersAPI
-userApi = Proxy
-
-server :: Server UsersAPI
-server = index :<|> create :<|> read' :<|> update :<|> delete'
-  where
-    index = pure mempty
-    create user = do
-      insertedUser <- runDB $ insertUnique user
-      case insertedUser of
-        Just _ -> pure user
-        Nothing -> throwError $ emailUsed (userMail user)
-    read' _id = do
-      gotUser <- runDB $ get (toSqlKey $ fromIntegral _id)
-      case gotUser of
-        Just user -> pure user
-        Nothing -> throwError $ err404 { errBody = "user with id = " <> (pack . show) _id <> "doesn't exist." }
-    update _id user = afterGet _id $ \key -> do
-      runDB $ repsert key user
-      pure mempty
-    delete' _id = afterGet _id $ \key -> do
-      runDB $ delete key
-      pure mempty
-
-err422 = ServantErr { errHTTPCode = 422, errBody = "", errReasonPhrase = "Unprocessable Entity", errHeaders = [] }
-emailUsed mail = err422 { errBody = "email address \"" <> (encodeUtf8 $ fromStrict $ mail) <> "\" is already used." }
-
-afterGet _id action = do
-    let key = toSqlKey $ fromIntegral _id :: Key User
-    gotUser <- runDB $ get key
-    case gotUser of
-      Just _ -> action key
-      Nothing -> throwError $ err404 { errBody = "user with id = " <> (pack . show) _id <> "doesn't exist." }
 
 main :: IO ()
-main = Warp.run 8080 $ serve userApi server
+main = Warp.run 8080 app
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,78 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+
+module Main (main) where
+
+import           Api (app)
+import           Test.Hspec
+import           Test.Hspec.Wai hiding (post, get, put, delete)
+import           Test.Hspec.Wai.JSON
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+commonHeaders = [("Content-Type", "application/json")]
+post path body = request "POST" path commonHeaders body
+get path = request "GET" path commonHeaders ""
+put path body = request "PUT" path commonHeaders body
+delete path = request "DELETE" path commonHeaders ""
+
+spec :: Spec
+spec = with (return app) $ do
+  describe "UsersAPI" $ do
+    let user1JSON = [json|
+                     {
+                       "userName": "Yusuke Sangenya",
+                       "userMail": "example1@example.com",
+                       "userPhoneNumber":"0123456789"
+                     }
+                   |]
+        user2JSON = [json|
+                     {
+                       "userName": "Yusuke Sangenya",
+                       "userMail": "example2@example.com",
+                       "userPhoneNumber":"0123456789"
+                     }
+                   |]
+
+    it "Create user" $ do
+      post "/" user1JSON `shouldRespondWith` 200
+      post "/" user2JSON `shouldRespondWith` 200
+
+    it "Get users" $ do
+      get "/" `shouldRespondWith` [json|
+           [{
+             "userName": "Yusuke Sangenya",
+             "userMail": "example1@example.com",
+             "userPhoneNumber":"0123456789"
+           },
+           {
+             "userName": "Yusuke Sangenya",
+             "userMail": "example2@example.com",
+             "userPhoneNumber":"0123456789"
+           }]
+         |]
+    
+    it "Get user" $ do
+      get "/1" `shouldRespondWith` [json|
+           {
+             "userName": "Yusuke Sangenya",
+             "userMail": "example1@example.com",
+             "userPhoneNumber":"0123456789"
+           }
+         |]
+
+    it "Delete user" $ do
+      delete "/2" `shouldRespondWith` 200
+      get "/2" `shouldRespondWith` 404
+
+    it "Update user" $ do
+      put "/1" user2JSON `shouldRespondWith` 200
+      get "/1" `shouldRespondWith` [json|
+          {
+            "userName": "Yusuke Sangenya",
+            "userMail": "example2@example.com",
+            "userPhoneNumber": "0123456789"
+          }
+        |]
+
